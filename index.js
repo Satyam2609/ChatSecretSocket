19931708c6c6bbadfd2c5860eb4d5290d61aceb2
Satyam Jain <satyam26092004@gmail.com>
2025-12-05 00:01:21 +0530

    user request1

--- index.js
+++ index.js
@@ -101,6 +101,14 @@ io.on("connection", async (socket) => {
     await group.save()
     io.to(creatorSocketId).emit("RequerstjoinRoom", {request:group.userRequest});
 
+        const messagesWithRoom = group.messages.map(msg => ({
+            usernamesend: msg.sender,
+            messagegee: msg.message,
+            timestamp: msg.timestamp,
+            roomIdIn: msg.groupName
+        }));
+        io.to(socket.id).emit("previousMessages", messagesWithRoom);
+
         // Send members & admin info to everyone
         const adminUser = group.creator;
         io.emit("members", { members: group.members, adminUserName: adminUser });
@@ -114,18 +122,6 @@ io.on("connection", async (socket) => {
             group.members.push(username)
             await group.save()
         }
-    })
-
-    socket.on("selectRoom" ,async ({ roomId})  => {
-        const group = await UserGroup.findOne({ groupName: roomId });
-         const messagesWithRoom = group.messages.map(msg => ({
-            username: msg.sender,
-            message: msg.message,
-            timestamp: msg.timestamp,
-            roomId
-        }));
-        io.to(socket.id).emit("previousMessages", messagesWithRoom);
-
     })
 
 
